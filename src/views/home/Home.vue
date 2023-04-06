ed9248c6dba5e4599348bf302878ca092d24f2b1
chenzhy01 <1027536483@qq.com>
2023-04-06 15:09:15 +0800

    update

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,61 @@
 <template>
-  <div>
-    <h2>主页</h2>
+  <div id="home">
+    <NavBarVue class="home-nav">
+      <template #center>
+        <div>购物街</div>
+      </template>
+    </NavBarVue>
+    <HomeSwiper :banners="this.banners"></HomeSwiper>
+    <RecommendView :recommends="this.recommends"></RecommendView>
+    <FeatureView></FeatureView>
   </div>
 </template>
 
 <script>
+import NavBarVue from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView.vue";
+
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+  components: {
+    NavBarVue,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+  },
 };
 </script>
 
 <style>
+#home {
+  padding-top: 44px;
+  padding-bottom: 49px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+}
 </style>
